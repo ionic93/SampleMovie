117deda880872ebd97cc0cbddaabb50ac401ee92
ionic93 <rhsfpem41@gmail.com>
2021-10-27 17:05:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/samplemovie/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.samplemovie;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    static RequestQueue queue;
+    EditText editText;
+    RecyclerView recyclerView;
+    MovieAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeRequest();
+            }
+        });
+
+        if (queue == null) queue = Volley.newRequestQueue(getApplicationContext());
+        recyclerView = findViewById(R.id.recyclerView);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new MovieAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void makeRequest() {
+        String url = editText.getText().toString();
+        StringRequest request = new StringRequest(
+            Request.Method.GET,
+            url,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    println("응답 -> " + response);
+                    processResponse(response);
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    println("에러 -> " + error.getMessage());
+                }
+            }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                return params;
+            }
+        };
+        request.setShouldCache(false);
+        queue.add(request);
+        println("요청 보냄.");
+    }
+
+    public void println(String data) {
+        Log.d("MainActivity", data);
+    }
+
+    public void processResponse(String response) {
+        Gson gson = new Gson();
+        MovieList movieList = gson.fromJson(response, MovieList.class);
+        println("영화정보의 수 : " + movieList.boxOfficeResult.dailyBoxOfficeList.size());
+        for (int i = 0; i < movieList.boxOfficeResult.dailyBoxOfficeList.size(); i++) {
+            Movie movie = movieList.boxOfficeResult.dailyBoxOfficeList.get(i);
+            adapter.addItem(movie);
+        }
+        adapter.notifyDataSetChanged();
+    }
+}
